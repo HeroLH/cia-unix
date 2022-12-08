cbd5c8b005c145cbe47db84ed20c59eb72eeaa5c
herolh <wait>
2022-12-08 09:46:14 +0800

    wait

--- decrypt_for_cia.sh
+++ decrypt_for_cia.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
 for ciaFile in `find . -name "*cia"` ; do
-  fullFilename=`basename $ciaFile`;
-  filename="${fullFilename%.*}";
+  fullFilename=`basename $ciaFile`;                           # xxx.cia
+  filename="${fullFilename%.*}";                              # xxx
   echo -e "\033[0;36mReady to decrypt file: $fullFilename \033[0m";
 
-  makerom -ciatocci $ciaFile
-  cciFullFilename="$filename.cci"
-  python decrypt.py $cciFullFilename >> log.txt 2>&1;
+  # decrypt .cia file, generate *.ncch files
+  python decrypt.py $fullFilename >> log.txt 2>&1;
+
+  # construct command for makerom
   declare -i i=0;
-  ARG="";
+  ARG="";                                                     # -i xxx.1.ncch:0:0 -i xxx.0.ncch:1:1
   for decryptFile in `find . -name "$filename.*.ncch"`; do
-    fullDecryptFilename=`basename $decryptFile`;
-      echo $fullDecryptFilename;
+    fullDecryptFilename=`basename $decryptFile`;              # xxx.ncch
       ARG="$ARG -i $fullDecryptFilename:$i:$i "
       i+=1;
   done
-  echo $ARG
-  echo `makerom -f cci -ignoresign -target p -o $filename-decrypt.cci $ARG`
+
+  # convert *.ncch to .cia
+  echo `makerom -f cia -ignoresign -target p -o $filename-decrypt.cia $ARG`
+
+  # dedlete *.ncch
   for decryptFile in `find . -name "$filename.*.ncch"`; do
     rm $decryptFile;
   done
-  rm $cciFullFilename;
-  echo -e "\033[0;32mDecrypted file successfully: $fullFilename \033[0m"
+
+  # check for success
+  if [ -f "$filename-decrypt.cia" ]; then
+    echo -e "\033[0;32mDecrypted file successfully: $fullFilename, generate file: $filename-decrypt.cia \033[0m"
+  else
+    echo -e "\033[0;31m Failed to decrypt the file, see log.txt for details \033[0m"
+  fi
 done
